18a68ac313e6e8598d78b8c6d0cf592d19f33f73
ZIM555 <zim555@mail.ru>
2014-07-31 21:34:09 +0300

    Init cyanogenmod tree

---
+++ logands/cm.mk
@@ -0,0 +1,19 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := GT-S7272
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/logands/device_logands.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := logands
+PRODUCT_NAME := cm_logands
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := GT-S7272
+
