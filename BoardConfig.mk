a2e3cb7e42804ff6466352d0a5d927718428c4e1
ZIM <zim555@mail.ru>
2015-07-16 21:02:40 +0300

    Add BACKLIGHT_PATH

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -112,6 +112,7 @@ BOARD_BATTERY_DEVICE_NAME := battery
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
 CHARGING_ENABLED_PATH := "/sys/class/power_supply/battery/batt_lp_charging"
+BACKLIGHT_PATH := "/sys/class/backlight/panel/brightness"
 
 # Sensor
 SENSORS_NEED_SETRATE_ON_ENABLE := true
