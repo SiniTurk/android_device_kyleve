341de75a560ae925fa1fe8001588bacc3ec0d4c3
ZIM <zim555@mail.ru>
2015-08-08 14:35:56 +0300

    Update init and charger

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -100,11 +100,11 @@ TARGET_BOOTANIMATION_PRELOAD := true
 TARGET_BOOTANIMATION_TEXTURE_CACHE := true
 
 # Charger
-BOARD_BATTERY_DEVICE_NAME := battery
-BOARD_CHARGER_ENABLE_SUSPEND := true
+#BOARD_BATTERY_DEVICE_NAME := battery
+#BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
-CHARGING_ENABLED_PATH := "/sys/class/power_supply/battery/batt_lp_charging"
-BACKLIGHT_PATH := "/sys/class/backlight/panel/brightness"
+#CHARGING_ENABLED_PATH := "/sys/class/power_supply/battery/batt_lp_charging"
+#BACKLIGHT_PATH := "/sys/class/backlight/panel/brightness"
 
 # healthd
 BOARD_HAL_STATIC_LIBRARIES := libhealthd-logands.hawaii
