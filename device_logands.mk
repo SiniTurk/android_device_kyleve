d808c172ced4e2cab73891d5b34c742fe2ed6744
ZIM <zim555@mail.ru>
2015-09-06 17:46:32 +0300

    Update config

--- device_logands.mk
+++ device_logands.mk
@@ -3,7 +3,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product-if-exists, vendor/samsung/logands/logands-common-vendor.mk)
 
 # Use high-density artwork where available
-#PRODUCT_LOCALES += hdpi
+PRODUCT_LOCALES += hdpi
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := hdpi
 
@@ -120,25 +120,22 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.multisim.config=dsds \
 	cm.updater.uri=http://get.ace3.tk \
 	ro.telephony.call_ring.multiple=0 \
+	camera2.portability.force_api=1 \
 	ro.telephony.call_ring=0
-	
-# Force use old camera api
-PRODUCT_PROPERTY_OVERRIDES += \
-    camera2.portability.force_api=1	
-    
+	  
 # enable Google-specific location features,
 # like NetworkLocationProvider and LocationCollector
-#PRODUCT_PROPERTY_OVERRIDES += \
-#    ro.com.google.locationfeatures=1 \
-#    ro.com.google.networklocation=1
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.locationfeatures=1 \
+    ro.com.google.networklocation=1
 
 # Extended JNI checks
 # The extended JNI checks will cause the system to run more slowly, but they can spot a variety of nasty bugs 
 # before they have a chance to cause problems.
 # Default=true for development builds, set by android buildsystem.
-#PRODUCT_PROPERTY_OVERRIDES += \
-#    ro.kernel.android.checkjni=0 \
-#    dalvik.vm.checkjni=false
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.kernel.android.checkjni=0 \
+    dalvik.vm.checkjni=false
 
 # MTP
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
