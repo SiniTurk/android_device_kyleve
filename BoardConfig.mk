ac6113b0da9b4f2b45785c04f7be0e16af024753
ZIM <zim555@mail.ru>
2016-02-01 15:51:42 +0200

    Enable WEBGL in WebKit

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -65,6 +65,9 @@ WIFI_BAND                   := 802_11_ABG
 TARGET_SCREEN_HEIGHT := 800
 TARGET_SCREEN_WIDTH := 480
 
+# Enable WEBGL in WebKit
+ENABLE_WEBGL                := true
+
 # Hardware rendering
 BOARD_EGL_CFG := device/samsung/logands/configs/egl.cfg
 USE_OPENGL_RENDERER := true
